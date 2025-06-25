d21f4b95e44360ab405242325ae02369ad5b1559
Shafeeq <122546228+IamShafeeq@users.noreply.github.com>
2025-06-25 20:17:22 +0100

    Update String3.java

--- String3.java
+++ String3.java
@@ -1,19 +1,4 @@
-/*
- Write a function that takes a list of strings an prints them, 
- one per line, in a rectangular frame. 
- For example the list ["Hello", "World", "in", "a", "frame"]
-  gets printed as:
 
-*********
-* Hello *
-* World *
-* in    *
-* a     *
-* frame *
-*********
-*/
-
-incomplete 
 
 
 class String3{
@@ -22,19 +7,7 @@ class String3{
 		printing("Hello shafeeq, are you ");
 
 	}
+	
 
-	public static void printing(String msg){
-		String word="";
-		int i=0;
-		for(;i<=msg.length() ;i++){			
-			if(i==msg.length() || msg.substring(i,i+1).equals(" ") ){
-				System.out.println("* "+word);
-				word="";
-			}else{
-				word+=msg.substring(i,i+1);
-			}
-		}
-		
 
-	}
 }
